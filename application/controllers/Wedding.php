5f8e778972a7c2a9186ed6bf01fa7199e5864dda
novaldyhh <novaldyhidayat@icloud.com>
2019-08-23 10:09:08 +0700

    update

--- application/controllers/Wedding.php
+++ application/controllers/Wedding.php
@@ -8,6 +8,7 @@ class Wedding extends CI_Controller
   public function __construct()
   {
     parent::__construct();
+    $this->load->model('Wedding_model')
     checkToken();
   }
 
@@ -19,6 +20,20 @@ class Wedding extends CI_Controller
   public function add()
   {
     render('wedding/add');
+    if($this->input->post('finish')){
+      $this->form_validation->set_rules('title', 'Judul', 'trim|required');
+      $this->form_validation->set_rules('pengantin_pria', 'Pengantin Pria', 'trim|required');
+      $this->form_validation->set_rules('pengantin_wanita', 'Pengantin Wanita', 'trim|required');
+      $this->form_validation->set_rules('tanggal', 'Tanggal', 'trim|required');
+      $this->form_validation->set_rules('waktu', 'Waktu', 'trim|required');
+      $this->form_validation->set_rules('tempat', 'Tempat', 'trim|required');
+      $this->form_validation->set_rules('alamat', 'Alamat', 'trim|required');
+      $this->form_validation->set_rules('tema', 'Tema', 'trim|required');
+      $this->form_validation->set_rules('hashtag', 'Hashtag', 'trim|required');
+      $this->form_validation->set_rules('penyelenggara', 'Penyelenggara', 'trim|required');
+      $this->form_validation->set_rules('undangan', 'Undangan', 'trim|required');
+      $this->form_validation->set_rules('status', 'Status', 'trim|required');
+    }
   }
 
   public function form()
