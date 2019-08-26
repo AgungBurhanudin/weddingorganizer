0637bacb0e9ec4ca0187e2f1e988497a339b9994
Novaldy <novaldyhidayat@icloud.com>
2019-08-26 08:12:22 +0700

    form_vendor

--- application/views/wedding/form_undangan.php
+++ application/views/wedding/form_undangan.php
@@ -24,20 +24,20 @@
         if (!empty($undangan)) {
             foreach ($undangan as $val) {
                 ?>
-                <tr>
-                    <td><?= $no++ ?></td>
-                    <td><?= $val->nama ?></td>
-                    <td><?= $val->alamat ?></td>
-                    <td><?= $val->tipe_undangan ?></td>
-                    <td>
-                        <a href="<?= base_url() ?>Wedding/undangan/edit?id=<?= $val->id ?>" class="btn btn-sm btn-primary"><i class="fa fa-pencil"></i> Edit</a>
-                        <a href="<?= base_url() ?>Wedding/undangan/delete?id=<?= $val->id ?>" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> Delete</a>                    
-                    </td>
-                </tr>
-                <?php
+        <tr>
+            <td><?= $no++ ?></td>
+            <td><?= $val->nama ?></td>
+            <td><?= $val->alamat ?></td>
+            <td><?= $val->tipe_undangan ?></td>
+            <td>
+                <a href="<?= base_url() ?>Wedding/undangan/edit?id=<?= $val->id ?>" class="btn btn-sm btn-primary"><i class="fa fa-pencil"></i> Edit</a>
+                <a href="<?= base_url() ?>Wedding/undangan/delete?id=<?= $val->id ?>" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> Delete</a>
+            </td>
+        </tr>
+        <?php
             }
         } else {
-            echo "<tr><td colspan='7'>Data Vendor Masih Kosong</td></tr>";
+            echo "<tr><td colspan='7'>Data Undangan Masih Kosong</td></tr>";
         }
         ?>
     </tbody>
@@ -53,17 +53,17 @@
                 </button>
             </div>
             <div class="modal-body">
-                <form class="form-horizontal" action="" method="post">
+                <form class="form-horizontal" action="" name="undangan" id="undangan" method="post">
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label" for="hf-email">Nama Lengkap</label>
                         <div class="col-md-9">
-                            <input name="nama_lengkap" id="nama_lengkap" type="text" required="required" class="form-control" placeholder="" />                            
+                            <input name="nama_lengkap" id="nama_lengkap" type="text" required="required" class="form-control" placeholder="" />
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label" for="hf-password">Alamat </label>
                         <div class="col-md-9">
-                            <input name="alamat_undangan" id="alamat_undangan" type="text" required="required" class="form-control"  />                            
+                            <input name="alamat_undangan" id="alamat_undangan" type="text" required="required" class="form-control" />
                         </div>
                     </div>
                     <div class="form-group row">
@@ -81,7 +81,7 @@
                         <label class="col-md-3 col-form-label" for="hf-password"></label>
                         <div class="col-md-9">
                             <button class="btn btn-secondary" type="button" data-dismiss="modal">Tutup</button>
-                            <button class="btn btn-primary" type="button">Simpan</button>
+                            <button class="btn btn-primary" type="submit" onclick="simpanUndangan()">Simpan</button>
                         </div>
                     </div>
                 </form>
@@ -91,3 +91,41 @@
     </div>
     <!-- /.modal-dialog-->
 </div>
+
+<script>
+    function simpanUndangan() {
+        var formData = new FormData($("#undangan")[0]);
+        $('#undangan').validate({
+            rules: {
+                nama: {
+                    required: true,
+                    minlength: 2
+                },
+            },
+            messages: {
+                nama: {
+                    required: "Please enter a Nama Undangan",
+                    minlength: "Nama Undangan minimal 2 karakter"
+                },
+            },
+            submitHandler: function(form) {
+                $.ajax({
+                    type: 'POST',
+                    url: '<?= base_url() ?>Wedding/undangan/add',
+                    processData: false,
+                    contentType: false,
+                    data: formData,
+                    dataType: "JSON",
+                    success: function(data) {
+                        if (data.code == "200") {
+                            swal("success", "Berhasil menambah undangan!");
+                            $("#myModal").modal('hide');
+                        } else {
+                            swal("warning", "Gagal menambah undangan!");
+                        }
+                    }
+                });
+            }
+        });
+    }
+</script>
